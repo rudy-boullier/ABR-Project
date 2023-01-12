c3c30f951c6f0dea827a3d34499920b8ee7264de
Vincent Chavot <vincent.chavot----dambrun@etu.univ-lyon1.fr>
2023-01-12 09:04:30 +0100

    fix : add title

--- Abr.py
+++ Abr.py
@@ -2,6 +2,8 @@ import math
 
 from Node import Node
 
+"""-------------------------------------- Add --------------------------------------"""
+
 
 def initAbr(values: list):
     abr = Node(values[0])
@@ -16,74 +18,7 @@ def addValues(abr: Node, values: list):
     return abr
 
 
-"""--------------Explor-----------------"""
-
-
-def prefix(abr: Node):
-    if abr is None:
-        return []
-    return [abr.val] + prefix(abr.left) + prefix(abr.right)
-
-
-def postfix(abr: Node):
-    if abr is None:
-        return []
-    return postfix(abr.left) + postfix(abr.right) + [abr.val]
-
-
-def infix(abr: Node):
-    if abr is None:
-        return []
-    return infix(abr.left) + [abr.val] + infix(abr.right)
-
-
-def width(abr: Node) -> list:
-    pile = [abr]
-    results = []
-    while len(pile):
-        node = pile.pop(0)
-        results.append(node.val)
-        if node.left is not None:
-            pile.append(node.left)
-        if node.right is not None:
-            pile.append(node.right)
-    return results
-
-
-def size(abr: Node) -> int:
-    if abr is None:
-        return 0
-    return 1 + size(abr.left) + size(abr.right)
-
-
-def height(abr: Node) -> int:
-    if abr is None:
-        return 0
-    return 1 + max(height(abr.left), height(abr.right))
-
-
-def researchAbr(abr: Node, value: float) -> bool:
-    if abr is None:
-        boolean = False
-    elif abr.val == value:
-        boolean = True
-    elif value < abr.val:
-        boolean = researchAbr(abr.left, value)
-    else:
-        boolean = researchAbr(abr.right, value)
-    return boolean
-
-
-def minAbr(abr: Node) -> float:
-    if abr.left is None:
-        return math.inf
-    return min(abr.val, minAbr(abr.left), minAbr(abr.right))
-
-
-def maxAbr(abr: Node) -> float:
-    if abr.right is None:
-        return -math.inf
-    return max(abr.val, maxAbr(abr.left), maxAbr(abr.right))
+"""-------------------------------------- Delete --------------------------------------"""
 
 
 def deleteExtremeRight(abr: Node) -> None:
@@ -132,3 +67,76 @@ def find_smallest(node):
     if node.left is not None:
         return find_smallest(node.left)
     return node
+
+
+""""-------------------------------------- Explorations --------------------------------------"""
+
+
+def prefix(abr: Node):
+    if abr is None:
+        return []
+    return [abr.val] + prefix(abr.left) + prefix(abr.right)
+
+
+def postfix(abr: Node):
+    if abr is None:
+        return []
+    return postfix(abr.left) + postfix(abr.right) + [abr.val]
+
+
+def infix(abr: Node):
+    if abr is None:
+        return []
+    return infix(abr.left) + [abr.val] + infix(abr.right)
+
+
+def width(abr: Node) -> list:
+    pile = [abr]
+    results = []
+    while len(pile):
+        node = pile.pop(0)
+        results.append(node.val)
+        if node.left is not None:
+            pile.append(node.left)
+        if node.right is not None:
+            pile.append(node.right)
+    return results
+
+
+def researchAbr(abr: Node, value: float) -> bool:
+    if abr is None:
+        boolean = False
+    elif abr.val == value:
+        boolean = True
+    elif value < abr.val:
+        boolean = researchAbr(abr.left, value)
+    else:
+        boolean = researchAbr(abr.right, value)
+    return boolean
+
+
+"""-------------------------------------- Features --------------------------------------"""
+
+
+def size(abr: Node) -> int:
+    if abr is None:
+        return 0
+    return 1 + size(abr.left) + size(abr.right)
+
+
+def height(abr: Node) -> int:
+    if abr is None:
+        return 0
+    return 1 + max(height(abr.left), height(abr.right))
+
+
+def minAbr(abr: Node) -> float:
+    if abr.left is None:
+        return math.inf
+    return min(abr.val, minAbr(abr.left), minAbr(abr.right))
+
+
+def maxAbr(abr: Node) -> float:
+    if abr.right is None:
+        return -math.inf
+    return max(abr.val, maxAbr(abr.left), maxAbr(abr.right))
